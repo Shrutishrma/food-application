9e0251301cf56930ebc4d625621a1d81f77887eb
Shrutishrma <136141975+Shrutishrma@users.noreply.github.com>
2025-11-30 21:15:17 +0530

    Fix SSL for Aiven

--- backend/index.js
+++ backend/index.js
@@ -116,15 +116,14 @@ app.use("/images", express.static(imagesDir));
 
 // DB CONNECTION (Render environment variables)
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: true
-    }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }
 });
+
 
 db.connect(err => {
     if (err) {
