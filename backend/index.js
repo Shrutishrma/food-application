b3bc4b3ac163f3d1efe8ec0e8e390678e0ede804
Shrutishrma <136141975+Shrutishrma@users.noreply.github.com>
2025-11-30 21:44:04 +0530

    Disable strict SSL verification for Aiven

--- backend/index.js
+++ backend/index.js
@@ -116,14 +116,15 @@ app.use("/images", express.static(imagesDir));
 
 // DB CONNECTION (Render environment variables)
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: true }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 
 db.connect(err => {
     if (err) {
