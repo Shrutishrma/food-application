af9642a73a11a7074a4aaa41a8784dd3ad12a508
Shrutishrma <136141975+Shrutishrma@users.noreply.github.com>
2025-12-02 01:00:30 +0530

    Fix Netlify SPA redirect

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+const API =
+  import.meta.env.VITE_API_URL ||
+  "https://food-application-pu9i.onrender.com"; // fallback if Netlify ENV fails
+
+console.log("Frontend using API:", API);
 
 const api = axios.create({
   baseURL: API,
